02462807d7c57618505dd48b345164a19b8a8397
KonstantinMishustin <122667389+KonstantinMishustin@users.noreply.github.com>
2024-06-22 18:31:08 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+class Car:
+
+    color = "color"
+    car_brand = "brand"
+
+    def go(self):
+        print("Go")
+
+    def stop(self):
+        print("Stop")
+
+    def info(self):
+        print("цвет машины: ", self.color, ", марка машины: ", self.car_brand)
+
+        
